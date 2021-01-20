057dc75051cf23b5188af9986afcf86cca93fe2e
sheriff olowolagba <sh3riff101@gmail.com>
2021-01-20 18:13:06 +0100

    Update ReactDynamicTableRender.js

--- ReactDynamicTableRender.js
+++ ReactDynamicTableRender.js
@@ -7,19 +7,12 @@
         - isImage :- An array of data that should be rendered as image
         - editWith : - A function. that is called to edit a row of data
         - delWith : - A function. that is called to delete a row of data
+//If Any Value in the table header is "Date" it the first 10 character hence, "2021-01-20T19:35:13.869Z" becomes "2021-01-20"
         
         
 import React from 'react';
 import { MdEdit, MdDelete } from "react-icons/md";
 import { TableContainer } from './TableStyle';
-
-/// Docs
-// const config = {
-//     uniqueId: "", // item dt has a uniqueId
-//     isImage: [], // item dt is an object
-//     editWith: "func", // function to edit
-//     delWith: "func",  // function to delete
-// }
 
 export const Table = ({ tableHeader, tableData, config }) => {
     const { uniqueId, isImage, editWith, delWith } = config;
@@ -43,6 +36,8 @@ export const Table = ({ tableHeader, tableData, config }) => {
                                         tableHeader.map(item =>
                                             (isImage.includes(item)) ?
                                             <td><img src={data[item]}  /></td> :
+                                            (item === "date") ?
+                                            <td>{data[item].slice(0,10)}</td> :
                                             <td>{data[item]}</td>
                                         )
                                     }
