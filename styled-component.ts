61730f9a61eb3660057dd4501f31755ae8e8ed2b
sheriff olowolagba <sh3riff101@gmail.com>
2021-03-14 20:36:26 +0100

    Update styled-component.ts

--- styled-component.ts
+++ styled-component.ts
@@ -1,5 +1,11 @@
 // Adding props at element level
   - const Whatever = styled.div <{ margin?: number }>`
+	    ${props => props.margin || 5 });
+	    ${({margin}) => margin || 5});
+    `;
+    ////// or //////
+  - type MarginType = { margin?: number;};
+  - const Whatever = styled.div<MarginType>`
 	    ${props => props.margin});
     `;
     ////// or //////
