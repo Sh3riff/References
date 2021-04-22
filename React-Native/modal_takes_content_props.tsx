cad4df7cc9ac3fbaee5c0465ad9f872198112670
sheriff olowolagba <sh3riff101@gmail.com>
2021-04-22 06:19:09 +0100

    Update modal_takes_content_props.tsx

--- React-Native/modal_takes_content_props.tsx
+++ React-Native/modal_takes_content_props.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {Modal, StyleSheet, View} from 'react-native';
-import {
-  heightPercentageToDP as hp,
-  widthPercentageToDP as wp,
-} from 'react-native-responsive-screen';
+import {Modal, StyleSheet, View, ScrollView} from 'react-native';
+import {Texts, Buttons} from '../components';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 type ModalProps = {
+  modalTitle: string;
   showModal: boolean;
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
   Content: () => JSX.Element;
+  ButtonList: {
+    type: 'primary' | 'secondary';
+    text: string;
+    operator: () => void;
+  };
 };
 
-const ModalView = ({showModal, setShowModal, Content}: ModalProps) => {
+const ModalView = ({
+  modalTitle,
+  showModal,
+  setShowModal,
+  Content,
+  ButtonList,
+}: ModalProps) => {
   return (
     <Modal
       animationType="slide"
@@ -20,7 +30,19 @@ const ModalView = ({showModal, setShowModal, Content}: ModalProps) => {
       onRequestClose={() => setShowModal(!showModal)}>
       <View style={styles.Background}>
         <View style={styles.modal}>
-          <Content />
+          <View style={styles.headerContainer}>
+            <Texts type="Title" content={modalTitle} />
+          </View>
+          <ScrollView>
+            <Content />
+          </ScrollView>
+          <View>
+            <Buttons
+              type={ButtonList.type}
+              text={ButtonList.text}
+              operator={ButtonList.operator}
+            />
+          </View>
         </View>
       </View>
     </Modal>
@@ -29,15 +51,23 @@ const ModalView = ({showModal, setShowModal, Content}: ModalProps) => {
 
 const styles = StyleSheet.create({
   Background: {
-    height: hp('80%'),
-    marginTop: hp('20%'),
+    height: hp('100%'),
+    backgroundColor: 'rgba(0,0,0,0.6)',
   },
   modal: {
-    flex: 1,
+    height: hp('80%'),
+    marginTop: hp('20%'),
     borderTopLeftRadius: hp('3%'),
     borderTopRightRadius: hp('3%'),
     backgroundColor: 'lightgrey',
-  }
+    justifyContent: 'space-between',
+  },
+  headerContainer: {
+    marginVertical: hp('4%'),
+  },
+  ButtonContainer: {
+    marginVertical: hp('1%'),
+  },
 });
 
 export default ModalView;
