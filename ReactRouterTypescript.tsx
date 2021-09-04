c40491295798c140fa884c9322f8f85cea484bec
sheriff olowolagba <contactme@sh3riff.me>
2021-09-04 01:04:52 +0100

    Update ReactRouterTypescript.tsx

--- ReactRouterTypescript.tsx
+++ ReactRouterTypescript.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Redirect} from "react-router-do
 import {useAuth} from './context'
 
 
+
+//////////////////////////////////////////////  Children //////////////////////////////////////////////
 const PrivateRoute = ({ children, ...rest }: {
     [x: string]: any;
     children: React.ReactNode;
@@ -49,4 +51,57 @@ const AuthRoute = ({ children, ...rest }: {
         }
       />
     );
+}
+
+//////////////////////////////////////////////  Component //////////////////////////////////////////////
+
+const BaseRoute = ({ children, ...rest }: {
+    [x: string]: any;
+    children: React.ReactNode;
+}) => {
+    const isAuth = true
+    return (
+      <Route
+          {...rest}
+          render={({ location }) =>
+            !isAuth ? (
+              children
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/",
+                  state: { from: location }
+                }}
+              />
+            )
+          }
+        />
+    );
+}
+
+const BaseRoute2 = ({ component: Component, ...rest }: any) => {
+    const isAuth = true
+    return (
+      <Route
+          {...rest}
+          render={(props) => !isAuth ? 
+            <Component {...props} /> : 
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { from: props.location }
+              }}
+            />}
+        />
+    );
+}
+
+
+const BackToTop = () => {
+  const urlPath = useLocation()
+  useEffect(() => {
+    (()=> window.scrollTo(0,0))()
+  }, [urlPath])
+
+  return null
 }
