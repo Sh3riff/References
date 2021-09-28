1d2e9320d1c4fd8dbb2eaee11e04c125461b8eeb
sheriff olowolagba <contactme@sh3riff.me>
2021-09-28 13:21:41 +0100

    Update useEffect-Cleanups.tsx

--- useEffect-Cleanups.tsx
+++ useEffect-Cleanups.tsx
@@ -30,6 +30,25 @@ function MyComponent() {
   // ...
 }
 
+const query = useQuery('todos', () => {
+   // Create a new CancelToken source for this request
+   const CancelToken = axios.CancelToken
+   const source = CancelToken.source()
+ 
+   const promise = axios.get('/todos', {
+     // Pass the source token to your request
+     cancelToken: source.token,
+   })
+ 
+   // Cancel the request if React Query calls the `promise.cancel` method
+   promise.cancel = () => {
+     source.cancel('Query was cancelled by React Query')
+   }
+ 
+   return promise
+ })
+
+
 
 //////////////////////////////////////////  Fetch requests  //////////////////////////////////////////
 
@@ -56,6 +75,25 @@ function MyComponent() {
 
   // ...
 }
+
+const query = useQuery('todos', () => {
+   // Create a new AbortController instance for this request
+   const controller = new AbortController()
+   // Get the abortController's signal
+   const signal = controller.signal
+ 
+   const promise = fetch('/todos', {
+     method: 'get',
+     // Pass the signal to your request
+     signal,
+   })
+ 
+   // Cancel the request if React Query calls the `promise.cancel` method
+   promise.cancel = () => controller.abort()
+ 
+   return promise
+ })
+
 
 
 //////////////////////////////////////////  Timer functions  //////////////////////////////////////////
