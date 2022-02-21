b8df5defa18ba7b0172e27365b4ee57fb72ecbde
sheriff olowolagba <sh3riff101@gmail.com>
2022-02-21 04:45:50 +0100

    Create reactNativeSvg.ts

---
+++ React-Native/reactNativeSvg.ts
@@ -0,0 +1,7 @@
+npm i react-native-svg
+npm i react-native-svg-transformer
+
+https://svg2jsx.com/
+https://react-svgr.com/playground/?native=true
+
+https://blog.logrocket.com/how-to-use-svgs-react-native-tutorial-with-examples/
