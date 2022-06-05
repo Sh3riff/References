4f967d2a5258755e1b50023459b98e19114551cc
sheriff olowolagba <sh3riff101@gmail.com>
2022-06-05 21:32:51 +0100

    Update styled-component.ts

--- styled-component.ts
+++ styled-component.ts
@@ -19,3 +19,26 @@
   - const Heading = styled.h1<{ active: boolean }>`
      color: ${props => (props.active ? 'red' : 'blue')};
     `;
+
+
+## CSS HELPER
+
+const commonStyle = css<{buttonVariant: 'primary' | 'secondary', marginRight: number}>`
+  all: unset;
+  background-color: ${({buttonVariant}) => buttonVariant === 'primary' ? '#191A1B' : '#E5E5E5'};
+  color: ${({buttonVariant}) => buttonVariant === 'primary' ? '#ffffff' : '#191A1B'};
+  padding: 12px 18px;
+  margin-right: ${({marginRight}) => marginRight + 'px'};
+  border-radius: 8px;
+  cursor: pointer;
+`;
+
+const LinkStyle = styled(Link)`
+  ${() => commonStyle}
+`;
+
+IF THE PROPS IS NEEDED, YOU MAY USE
+
+const ButtonStyle = styled.button`
+  ${(props) => prope.primary ? "this" : "that"}
+`;
